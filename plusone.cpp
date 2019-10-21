9b6f0df1cf36cda0d125770b9aa648582e2ae0a3
kwame3 <kwame3@umbc.edu>
2019-10-20 21:55:35 -0400

    Update plusone.cpp

--- plusone.cpp
+++ plusone.cpp
@@ -1,3 +1,4 @@
+// https://www.reddit.com/r/dailyprogrammer/comments/aphavc/20190211_challenge_375_easy_print_a_new_number_by/
 // A number is input in computer then a new no should get printed by adding one to each of its digit.
 // If you encounter a 9, insert a 10 (don't carry over, just shift things around).
 // For example, 998 becomes 10109.
